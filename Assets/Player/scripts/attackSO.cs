e36cfb6f316c66028a950ca195057369c413c173
squirreImancer <jjwv802@gmail.com>
2026-09-14 01:53:04 -0400

    [p

--- Assets/Player/scripts/attackSO.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(menuName ="Attacks/Normal")]
-public class attackSO : ScriptableObject
-{
-    public AnimatorOverrideController OVcontroller;
-    public float damage;
-}
